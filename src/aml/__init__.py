241515c2ec3776ba5388762576451241e7e60c11
artwmana <artwmana@gmail.com>
2025-11-29 20:15:41 +0300

    Adding __init__ file

---
+++ src/aml/__init__.py
@@ -0,0 +1,3 @@
+from .data import optimize_dataframe, flatten_metadata
+
+__all__ = ["optimize_dataframe", "flatten_metadata"]
---
+++ src/aml/data/__init__.py
@@ -0,0 +1,4 @@
+from .dtype_downcasting import optimize_dataframe
+from .json_extractor import flatten_metadata
+
+__all__ = ["optimize_dataframe", "flatten_metadata"]
